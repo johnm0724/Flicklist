37044188347a1b6ca4e1dc74df6d9b9f3215132b
phylandroid <phylishanoel@gmail.com>
2021-07-19 07:44:56 -0500

    test commit
    trying vsCode and git desktop out

--- Flicklist/flickListMovie/Flicklist/src/main/java/users.java
+++ Flicklist/flickListMovie/Flicklist/src/main/java/users.java
@@ -1,4 +1,32 @@
 
-public class users {
+
+public class User {
+	
+	public static String username;
+	public static String password;
+	
+	//getters & setters
+	public static String getUsername() {
+		return username;
+	}
+	public static void setUsername(String username) {
+		User.username = username;
+	}
+	public static String getPassword() {
+		return password;
+	}
+	public static void setPassword(String password) {
+		User.password = password;
+	}
+	
+	
+	public void reviewMovie() {
+		
+	}
+	
+	public void likeMovie() {
+		
+	}
+	
 
 }
